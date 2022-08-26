c2197d4c0fa17a61bb5f6261069cabfe614ba33f
HitmanAlharbi <36328177+HitmanAlharbi@users.noreply.github.com>
2022-08-26 13:02:21 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
 if(!empty($_FILES["f"]["name"])){
     $n = basename($_FILES["f"]["name"]); 
     $e = pathinfo($n, PATHINFO_EXTENSION); 
-    $a = array('json','php','sqlite','txt','db'); 
+    $a = array('json','sqlite','txt','db'); 
     if(in_array($e, $a)){ 
         $f = $_FILES['f']['tmp_name']; 
         $c = file_get_contents($f); 
